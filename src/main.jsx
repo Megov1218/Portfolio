3be35055c1266de3b4d3e4ab5990ed38764f2ce8
Megov1218 <govindwarmeet1224@gmail.com>
2026-02-14 18:46:20 +0530

    netlify identity

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// ✅ Netlify Identity Init
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("recovery", () => {
+        window.netlifyIdentity.open();
+      });
+    }
+  });
+  window.netlifyIdentity.init();
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
